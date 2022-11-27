f4dd8734bdf48721d5037b62599e30154063aa17
const <enogue50@gmail.com>
2022-11-27 21:59:27 +0300

    attempt 4: edit prime.js

--- src/prime.js
+++ src/prime.js
@@ -6,7 +6,10 @@ const userName = inputName();
 let countOfQuestion = 0;
 console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
 export const gamePrime = () => {
-    const num1 = getRandom(50);
+    let num1 = getRandom(50);
+    if (num1 === 0) {
+        num1 += 1;
+    }
     const correctAnswer = isPrimary(num1);
     console.log(`Question: ${num1}`);
     const userAnswer = readlineSync.question(`Your answer: `);
